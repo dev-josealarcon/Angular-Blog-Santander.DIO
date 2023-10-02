c23c65c171b29c929678e56b6323f82ee1e90378
José Alarcón <dev.josealarcon@gmail.com>
2023-10-02 19:32:07 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -10,7 +10,8 @@ export class SmallCardComponent implements OnInit {
   photoCover:string='';
   @Input()
   cardTitle:string = '';
-
+  @Input()
+  Id:String="0";
 
   constructor() { }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOVO HOMEN DE FERRO ANUNCIADO",
+    "description": "Marvels Studios anuncia novo filme do homen de ferro.",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6xTwdqgz3yQdICaPpdBj-GqPlc3aLd9ojw&usqp=CAUhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6xTwdqgz3yQdICaPpdBj-GqPlc3aLd9ojw&usqp=CAU"
+  },
+  {
+    "id": "2",
+    "title": "Saiu nova serie no Disney+",
+    "description": "blablabla",
+    "photoCover":"https://nosbastidores.com.br/wp-content/uploads/2021/08/Series-Marvel-Disney-Plus-1024x576-1.jpg"
+  },
+];
